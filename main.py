5a6e4b88c87d05fe7da5e98dc622ad9896cc03d8
Kai Sun <autosunkai@gmail.com>
2021-08-01 21:55:38 +0800

    👌 UPDATE: Update main.

--- main.py
+++ main.py
@@ -12,9 +12,10 @@
     @updated: 2021/8/1 21:08
 """
 from bubble_sort import *
+from selection_sort import *
 
 if __name__ == '__main__':
-    sort = bubble_sort_pro
+    sort = selection_sort
     arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
     print('Raw:', arr)
     arr = sort(arr)
